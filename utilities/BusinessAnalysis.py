c5c3e2246a9397b8f65c9286e48511c44b5057e9
shreyansbardia <shreyansbardia62@gmai.com>
2023-11-26 14:44:50 +0530

    Files added

--- utilities/BusinessAnalysis.py
+++ utilities/BusinessAnalysis.py
@@ -2,20 +2,16 @@
 import pandas as pd
 import streamlit as st
 
-class CustomerAnalysis:
-    def __init__(self, customer_id):
-        self.customer_id = customer_id
-        self.account_data = self.load_data('accounts.xlsx')
-        self.customer_data = self.load_data('customers.xlsx')
-        self.fd_data = self.load_data('fixed_deposits.xlsx')
+class BusinessAnalysis:
+    def __init__(self):
+        self.account_data = self.load_data('account.xlsx')
+        self.customer_data = self.load_data('customer.xlsx')
+        self.fd_data = self.load_data('fixed_deposit.xlsx')
         self.investment_data = self.load_data('investment_accounts.xlsx')
         self.loans_data = self.load_data('loans.xlsx')
         self.mf_data = self.load_data('mutual_funds.xlsx')
         self.stocks_data = self.load_data('stocks.xlsx')
         self.transactions_data = self.load_data('transactions.xlsx')
-
-        # Filter data based on customer ID
-        # Repeat for other datasets
 
     def load_data(self, file_path):
         data = pd.read_excel(file_path)
