e685ad4698311698444a9691a1511306413ceee7
shreyansbardia <shreyansbardia62@gmai.com>
2023-11-28 15:14:13 +0530

    Business changes

--- utilities/BusinessAnalysis.py
+++ utilities/BusinessAnalysis.py
@@ -15,9 +15,6 @@ class BusinessAnalysis:
         self.stocks_data = self.load_data('Stocks')
         self.transactions_data = self.load_data('Transactions')
 
-        # Filter data based on customer ID
-        # Repeat for other datasets
-
     def load_data(self, file_path):
         data = self.sqlloader.load_table(file_path)
         # Additional data validation if needed
